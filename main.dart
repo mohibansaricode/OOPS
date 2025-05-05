34eb72b62a6be645a83914ced84387bbdca25b8d
mohibansaricode <mmcode46@gmail.com>
2025-05-06 02:03:02 +0500

    contractors

--- main.dart
+++ main.dart
@@ -1,3 +1,12 @@
-void main(){
-  
+void main() {
+  Person obj = Person("mohib", 20);
+  print(obj.name);
+  print(obj.age);
+}
+
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
 }
