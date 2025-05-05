cd6ae88fd0d14f08a4718a79cb8b657cff4f22c4
mohibansaricode <mmcode46@gmail.com>
2025-05-06 02:08:16 +0500

    with functions

--- main.dart
+++ main.dart
@@ -1,12 +1,25 @@
 void main() {
-  Person obj = Person("mohib", 20);
-  print(obj.name);
-  print(obj.age);
+  //   Person obj = Person("mohib", 20);
+  //   print(obj.name);
+  //   print(obj.age);
+  // }
+
+  // class Person {
+  //   String name;
+  //   int age;
+
+  //   Person(this.name, this.age);
+  Person obj = Person();
+  obj.isSleep();
 }
 
 class Person {
-  String name;
-  int age;
+  String name = "mohib";
+  int age = 20;
 
-  Person(this.name, this.age);
+  isSleep() {
+    print("$name is Sleeping");
+  }
+
+  isEat() {}
 }
